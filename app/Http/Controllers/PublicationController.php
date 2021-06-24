97114c55a8c69af4dadff7f653ac7265507296c5
Jonathan141999 <jonathan.alquinga@epn.edu.ec>
2021-06-24 11:05:36 -0500

    Validacion de Datos realizada

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -6,6 +6,8 @@ use App\Models\Publication;
 use App\Http\Resources\Publication as PublicationResource;
 use App\Http\Resources\PublicationCollection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class PublicationController extends Controller
 {
@@ -19,6 +21,19 @@ class PublicationController extends Controller
     }
     public function store(Request $request)
     {
+        $messages = [
+            'required' => 'El campo :attribute es obligatorio.',
+        ];
+
+        $request->validate([
+            'affair' => 'required|string|unique:publications|max:255',
+            'details' => 'required|string|max:255',
+            'hour' => 'required|string',
+            'location' => 'required|string',
+            'phone' => 'required|string',
+            'date' => 'required|date',
+            'category_id' => 'required|exists:categories,id',
+        ],$messages);
         $publication = Publication::create($request->all());
         return response()->json($publication, 201);
     }
